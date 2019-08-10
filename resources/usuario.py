38b89f88369678640fa1cc4cbf67a7bc91849c35
sserafim <samuel.serafim2010@gmail.com>
2019-08-09 21:29:58 -0300

    commit

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,14 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token
+
+#global variables
+attr = reqparse.RequestParser()
+attr.add_argument('login', type=str, required=True,
+                  help='The field login cannot be left blank')
+attr.add_argument('senha', type=str, required=True,
+                  help="The senha cannot be left blank")
 
 
 class User(Resource):
@@ -24,11 +33,6 @@ class User(Resource):
 class UserRegister(Resource):
     # /cadastro
     def post(self):
-        attr = reqparse.RequestParser()
-        attr.add_argument('login', type=str, required=True,
-                          help='The field login cannot be left blank')
-        attr.add_argument('senha', type=str, required=True,
-                          help="The senha cannot be left blank")
 
         dados = attr.parse_args()
         print(dados['login'])
@@ -39,3 +43,16 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return{'message': 'User created successfully'}, 201
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = attr.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incirrect'}, 401
