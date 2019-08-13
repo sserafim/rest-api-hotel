7e01fbbd6940c07a03fdd8f524ea51b6a718eb87
sserafim <samuel.serafim2010@gmail.com>
2019-08-12 22:30:07 -0300

    Commit

--- models/usuario.py
+++ models/usuario.py
@@ -1,4 +1,6 @@
 from sql_alchemy import banco
+from flask import request, url_for
+from requests import post
 
 
 # Cria um objeto model da clase HotelModel
@@ -7,14 +9,31 @@ class UserModel(banco.Model):
     __tablename__ = 'usuarios'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(40))
-    senha = banco.Column(banco.String(40))
+    login = banco.Column(banco.String(40), nullable=False, unique=True)
+    senha = banco.Column(banco.String(40), nullable=False)
+    email = banco.Column(banco.String(80), nullable=False, unique=True)
     ativado = banco.Column(banco.Boolean, default=False)
 
-    def __init__(self, login, senha):
+    def __init__(self, login, senha, email, ativado):
         self.login = login
         self.senha = senha
+        self.email = email
         self.ativado = ativado
+
+    def send_confirmation_email(self):
+        # Monta link para enviar no e-mail confirmar.
+        link = request.url_root[:-1] + \
+            url_for('userconfirm', user_id=self.user_id)
+        return post('https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DMAIN),
+                    auth=('api', MAILGUN_API_KEY),
+                    data={'from': '{} <{}>'.format(FROM_TILE, FROM_EMAIL),
+                          'to': self.email,
+                          'subject': 'Confirmação de Cadastro',
+                          'text': 'Confirme seu cadastro clicando no link a seguir: {}'.format(link),
+                          'html': '<html><p>\
+                              Confirme seu cadastro clicndo no link a seguir: <a href="{}">CONFIRMAR EMAIL</a>\
+                                  </p></html>'.format(link)
+                          })
 
     # Funcão que Transforma um objeto em um fomato DICIONÁRIO que converte/ou é um JSON - automaticamente >>>  <<<
 
@@ -22,6 +41,7 @@ class UserModel(banco.Model):
         return {
             'user_id': self.user_id,
             'login': self.login,
+            'email': self.email,
             'ativado': self.ativado
         }
 
