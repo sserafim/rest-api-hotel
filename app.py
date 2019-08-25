22bc5214bfcac22a6580e5c00d280ad1285c2b0d
sserafim <samuel.serafim2010@gmail.com>
2019-08-25 12:28:26 -0300

    Commit

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ api.add_resource(UserConfirm, '/confirmacao/<int:user_id>')
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
